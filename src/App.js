c8a6e9556f3046f0b6ec5d62b7a647e05f38cfa1
harsh-bardolia <hbardolia2002@gmail.com>
2023-02-16 15:19:05 +0530

    Taken the task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index";
 
 function App() {
+  const [input, setInput] = useState("");
+  // console.log(input);
+  const [task, setTask] = useState([]);
+
+  // console.log(task);
+
+  const addTask = (input) => {
+    setTask([...task, input]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div className="flex flex-col items-center justify-center m-5   ">
+        <div className="flex ">
+          <input
+            className="py-3 px-6 "
+            placeholder="Enter the Task"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              addTask(input);
+              setInput('');
+            }}
+            className="text-white bg-red-400 py-3 px-6"
+          >
+            Submit
+          </button>
+        </div>
+
+        <div className="flex flex-col py-2">
+          {task && (
+            <div className="border b-2 px-6 py-3 rounded-lg m-5 text-white">
+              {task}
+            </div>
+          )}
+
+        </div>
+      </div>
     </div>
   );
 }
